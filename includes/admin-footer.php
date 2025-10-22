abf671a66a5a65540113dff58ed17187c6ff3e6a
gomedo <gomedo8080@gmail.com>
2025-10-23 12:33:04 +1300

    Add admin footer with Bootstrap and auto-dismiss alerts

---
+++ includes/admin-footer.php
@@ -0,0 +1,14 @@
+   </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // Auto-dismiss alerts
+        setTimeout(function() {
+            let alerts = document.querySelectorAll('.alert');
+            alerts.forEach(function(alert) {
+                let bsAlert = new bootstrap.Alert(alert);
+                bsAlert.close();
+            });
+        }, 5000);
+    </script>
+</body>
+</html>
